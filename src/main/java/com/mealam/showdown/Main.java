1dfa73bacd180edd05f1923c78f80039420f2ace
Aram <schonhagearam@gmail.com>
2025-12-11 14:27:56 +0100

    Functional Static API for dragons in DOB, make more global soon

--- src/main/java/com/mealam/showdown/Main.java
+++ src/main/java/com/mealam/showdown/Main.java
@@ -9,17 +9,35 @@ package com.mealam.showdown;
 
 
 import com.mealam.showdown.loader.LoadInitializer;
+import com.mealam.showdown.utils.logging.LogLevel;
+import com.mealam.showdown.utils.logging.Logger;
 import io.javalin.Javalin;
+
+import java.io.InputStream;
 
 public class Main {
 
 	public static Javalin Main;
-	
+
 	public static void main(String[] pArgs) {
 		LoadInitializer.init();
 
 		Main = Javalin.create(/*config*/)
 				.get("/", ctx -> ctx.result("Hello World"))
+				.get("static/api/dragons/dob/{name}", ctx -> {
+					String name = ctx.pathParam("name");
+					String resourcepath = "/static/api/dragons/dob/" + name + ".json";
+
+					try (InputStream in = com.mealam.showdown.Main.class.getResourceAsStream(resourcepath)) {
+						if (in == null) {
+							ctx.status(404).result("Not Found");
+							return;
+						}
+						String json = new String(in.readAllBytes());
+						ctx.contentType("application/json");
+						ctx.result(json);
+					}
+				})
 				.start(7070);
 	}
 }
